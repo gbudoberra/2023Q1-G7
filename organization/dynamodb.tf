22bec7fc32c0113365fc444ed268fcf3ac6166af
fzimbimbakis <fzimbimbakis@itba.edu.ar>
2023-06-22 19:26:22 -0300

    modulos externos movidos a modules

--- organization/dynamodb.tf
+++ organization/dynamodb.tf
@@ -1,9 +1,10 @@
 module "dynamodb_table" {
-  source = "terraform-aws-modules/dynamodb-table/aws"
+  source = "../modules/dynamodb"
 
   for_each = local.dynamodb.tables
 
   name      = each.value.name
+
   hash_key  = each.value.hash_key
   range_key = each.value.range_key
 
@@ -11,8 +12,5 @@ module "dynamodb_table" {
   tags       = each.value.tags
 
   global_secondary_indexes = each.value.global_secondary_indexes
-
-  server_side_encryption_enabled = true
-  table_class                    = "STANDARD"
 
 }
